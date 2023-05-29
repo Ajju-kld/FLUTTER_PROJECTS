c593ef409a14b7ca583bd74f7b3d2911f28720fb
Muhammed Ajmal <mohammedajmal996@gmail.com>
2023-05-29 10:50:36 +0530

    added fingerprint authenticate

---
+++ fingerprint_authentication/lib/homescreen.dart
@@ -0,0 +1,104 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final auth = LocalAuthentication();
+  String authorized = 'not authorized';
+
+  Future<void> _authenticate() async {
+    bool authenticated = false;
+    try {
+      authenticated = await auth.authenticate(
+          localizedReason:
+              'Scan your fingerprint (or face or whatever) to authenticate',
+          options: const AuthenticationOptions(
+              biometricOnly: true, stickyAuth: true));
+    } on PlatformException catch (e) {
+      print(e);
+    }
+
+    setState(() {
+      authorized =
+          authenticated ? 'authorized success' : 'failed to authenticate';
+    });
+    print(authorized);
+  }
+
+  @override
+  void initState() {
+
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey.shade600,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Center(
+              child: Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 48.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 50.0),
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 15.0),
+                    child: const Text(
+                      "Authenticate using your fingerprint instead of your password",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.white, height: 1.5),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(vertical: 15.0),
+                    width: double.infinity,
+                    child: FloatingActionButton(
+                      onPressed: () {
+                        _authenticate();
+                      },
+                      elevation: 0.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 24.0, vertical: 14.0),
+                        child: Text(
+                          "Authenticate",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
